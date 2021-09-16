b3288635ebb221d0ea1067e682149889d5df0a11
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-09-15 23:04:13 -0700

    Allow collecting emails.

--- misc/filter_people.py
+++ misc/filter_people.py
@@ -2,6 +2,7 @@
 
 python -m misc.filter_people --task "find_people_with_no_enter_year"
 python -m misc.filter_people --task "find_people_with_no_sponsor"
+python -m misc.filter_people --task "collect_emails"
 """
 
 import argparse
@@ -11,10 +12,12 @@ import os
 from .common import core_faculty
 
 
-def find_people_with_no_enter_year(record, ignore_faculty=True, only_email=True):
+def find_people_with_no_enter_year(record, ignore_not_current=True, ignore_faculty=True, only_email=True):
     out = [entry for entry in record if len(entry["year"]) == 0]
     if ignore_faculty:
         out = [entry for entry in out if entry["status"] != "faculty"]
+    if ignore_not_current:
+        out = [entry for entry in out if entry["current"] == 'x']
     if only_email:
         out = [entry["email"] for entry in out]
     print('people with no enter year:')
@@ -42,6 +45,22 @@ def find_people_with_no_sponsor(record, ignore_not_current=True, ignore_faculty=
         out = [entry["email"] for entry in out]
     print('people with no sponsor:')
     # For easier copy-paste into email.
+    for outi in out:
+        print(outi)
+    return out
+
+
+def collect_emails(record, ignore_not_current=True, ignore_faculty=True):
+    """Collect emails of people on the roster from the tsv file."""
+    out = []
+    for entry in record:
+        if ignore_faculty and entry["status"] == "faculty":
+            continue
+        if ignore_not_current and entry['current'] != 'x':
+            continue
+        out.append(entry)
+    out = [entry["email"] for entry in out]
+    print('emails: ')
     for outi in out:
         print(outi)
     return out
@@ -94,12 +113,30 @@ def table2dicts(table_path):
     return record
 
 
-def main(task, table_path):
-    record = table2dicts(table_path)
-    if task == "find_people_with_no_enter_year":
-        find_people_with_no_enter_year(record)
-    elif task == "find_people_with_no_sponsor":
-        find_people_with_no_sponsor(record)
+def main():
+    record = table2dicts(args.table_path)
+    if args.task == "find_people_with_no_enter_year":
+        find_people_with_no_enter_year(
+            record, ignore_faculty=args.ignore_faculty, ignore_not_current=args.ignore_not_current
+        )
+    elif args.task == "find_people_with_no_sponsor":
+        find_people_with_no_sponsor(
+            record, ignore_faculty=args.ignore_faculty, ignore_not_current=args.ignore_not_current
+        )
+    elif args.task == "collect_emails":
+        collect_emails(
+            record, ignore_faculty=args.ignore_faculty, ignore_not_current=args.ignore_not_current
+        )
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    if v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 if __name__ == "__main__":
@@ -111,8 +148,10 @@ if __name__ == "__main__":
              "https://docs.google.com/spreadsheets/d/1W9D5NFxOXuzpao7aB6BXTnj5uhyPAJXBj9oLkF7IAnc/edit#gid=0"
     )
     parser.add_argument('--task', type=str,
-                        choices=("find_people_with_no_enter_year", "find_people_with_no_sponsor"),
+                        choices=("find_people_with_no_enter_year", "find_people_with_no_sponsor", "collect_emails"),
                         required=True)
+    parser.add_argument('--ignore_faculty', type=str2bool, default=False, const=True, nargs="?")
+    parser.add_argument('--ignore_not_current', type=str2bool, default=False, const=True, nargs="?")
     args = parser.parse_args()
 
-    main(task=args.task, table_path=args.table_path)
+    main()
